44f1874d1b88107da0fe6b53f3a168a3f347de69
Josguther1234 <josguther@mesoamericana.edu.gt>
2019-11-20 19:40:29 -0600

    Creacion de Modelos

--- restaurante/models.py
+++ restaurante/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib import admin
+
+
+class Menu(models.Model):
+    nombre =  models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Plato(models.Model):
+    nombre = models.CharField(max_length=60)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class PlatoAdmin(admin.ModelAdmin):
+    inlines = (CartaInLine,)
+
+class MenuAdmin(admin.ModelAdmin):
+    inlines = (CartaInLine,)
